52493ac39545e217f87d65ee56913d0acdcbbfe5
Haliam <haliampf@gmail.com>
2021-03-20 10:26:14 +0000

    Add project files.

---
+++ WebAPIClientDotnetRepo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WebAPIClientDotnetRepo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
