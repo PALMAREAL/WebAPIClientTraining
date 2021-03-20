a1b9b0c5e0a1fd005a44778cdf440915f15df23b
Haliam <haliampf@gmail.com>
2021-03-20 10:49:08 +0000

    feature/implementing ProcessRepositories method and the endpoint

--- WebAPIClientDotnetRepo/Program.cs
+++ WebAPIClientDotnetRepo/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 
 namespace WebAPIClientDotnetRepo
@@ -15,7 +16,18 @@ namespace WebAPIClientDotnetRepo
 
         private static async Task ProcessRepositories()
         {
+            client.DefaultRequestHeaders.Accept.Clear();
 
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+
+            client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+            var stringTask = client.GetStringAsync("https://api.github.com/orgs/dotnet/repos");
+
+            var msg = await stringTask;
+
+            Console.Write(msg);
         }
     }
 }
